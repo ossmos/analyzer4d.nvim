0ebd1b63a87b39ba7f8d0e3283bd3f6055230d6d
Ossmos <flashthemusic@gmx.de>
2024-10-14 21:02:17 +0200

    Add function to remove characters before the json string

--- lua/analyzer4d/utils.lua
+++ lua/analyzer4d/utils.lua
@@ -1,5 +1,14 @@
 local M = {}
 
+function M.remove_json_prefix(str)
+    local i = string.find(str, "{", 1, true)
+    if i == nil then
+        return str
+    end
+    return string.sub(str, i)
+end
+
+
 function M.split_string(str, separator)
     separator = separator or "%s"
     local t = {}
